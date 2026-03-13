78c59691941ab8963d04bc0c7309b2268f50b819
surinder <mesingh9719@gmail.com>
2026-03-13 08:43:27 +0530

    Fix: Use production settings in Vercel entry point

--- api/index.py
+++ api/index.py
@@ -1,11 +1,8 @@
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hrms_lite.settings')
+# Use production settings on Vercel
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hrms_lite.settings.production')
 
-from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 app = get_wsgi_application()
-
-if os.getenv('VERCEL'):
-    call_command('migrate', interactive=False, run_syncdb=True, verbosity=0)
